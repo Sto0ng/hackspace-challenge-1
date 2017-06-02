c5d3afbf10e546fe910067260725eef642ecc7c1
Robert Alonso <robert.alonso.ac@gmail.com>
2017-06-02 01:41:44 -0500

    Add routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { UserDescriptionComponent } from './user-list/user-description/user-description.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+const routes: Routes = [
+  {path: 'detail', component: UserDescriptionComponent},
+  {path: 'list', component: UserListComponent;}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDescriptionComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
